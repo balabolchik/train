69f46b2f9a9a2627e09a4197d539fa858f0d10e0
balabolchik <obordulyak@gmail.com>
2020-11-01 16:39:28 +0200

    finish

--- js/index.js
+++ js/index.js
@@ -1,38 +1,11 @@
-// let dog = {
-//   name: "Chappie",
-//   legs: 4,
-//   isAwesome: true,
-//   sound: "Гав-Гав",
-//   speak: speaking
-// };
-
-// dog.speak();
-
-// function speaking () {
-//   console.log(`${this.sound}! Меня зовут ${this.name}!`);
-// };
-
-// let pig = {
-//   sound: "Хрю",
-//   name: "Чарли",
-//   speak: speaking
-// };
-
-// let horse = {
-//   sound: "И-го-го",
-//   name: "Мэри",
-//   speak: speaking
-// };
-
-// pig.speak();
-// horse.speak();
-let Car = function (x,y) {
+let Car = function (x,y,z) {
   this.x = x;
   this.y = y;
+  this.speed = z;
 };
 
 Car.prototype.draw = function () {
-  let carHtml = '<img src="../img/car.png">';
+  let carHtml = `<img src="../img/car.png">`
   this.carElement = $(carHtml);
   
   this.carElement.css({
@@ -43,9 +16,44 @@ Car.prototype.draw = function () {
   $("body").append(this.carElement);
 };
 
+Car.prototype.moveRight = function () {
+  this.x += this.speed;
+  this.carElement.css({
+    left: this.x,
+    top: this.y
+  });
+};
 
-let tesla = new Car(20,20);
+Car.prototype.moveLeft = function () {
+  this.x -= this.speed;
+  this.carElement.css({
+    left: this.x,
+    top: this.y
+  });
+};
+
+Car.prototype.moveUp = function () {
+  this.y -= this.speed;
+  this.carElement.css({
+    left: this.x,
+    top: this.y
+  });
+};
+
+Car.prototype.moveDown = function () {
+  this.y += this.speed;
+  this.carElement.css({
+    left: this.x,
+    top: this.y
+  });
+};
+
+
+let tesla = new Car(20,20,5);
 tesla.draw();
 
-let nissan = new Car(100,200);
+let nissan = new Car(20,90,20);
 nissan.draw();
+
+let reno = new Car(20, 160, 15);
+reno.draw();
